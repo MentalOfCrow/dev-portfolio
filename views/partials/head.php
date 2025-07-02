f4f935985b4eaec3d789602be515c5e5293a5807
MentalOfCrow <145018989+MentalOfCrow@users.noreply.github.com>
2025-07-02 16:20:41 +0200

    header

--- views/partials/head.php
+++ views/partials/head.php
@@ -21,8 +21,4 @@
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
-
-<body>
-    <!-- Rest of the file remains unchanged -->
-</body>
-</html> 
+<body> 
